db3b961335d99ec0c4bd497615039644fdd16038
unknown <rico_1882@hotmail.com>
2026-02-11 22:00:53 -0300

    AAA (Arrange, Act, Assert)

--- playwright/e2e/pedidos.spec.ts
+++ playwright/e2e/pedidos.spec.ts
@@ -1,22 +1,24 @@
 import { test, expect } from '@playwright/test';
 
-test('deve consultar um pedido aprovado', async ({ page }) => {
-  await page.goto('http://localhost:5173/');
+///AAA (Arrange, Act, Assert)
 
-  //checkpoint
+test('deve consultar um pedido aprovado', async ({ page }) => {
+  
+  //Arrange
+  await page.goto('http://localhost:5173/');
   await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint');
 
   await page.getByRole('link', { name: 'Consultar Pedido' }).click();
-
-  //checkpoint
   await expect(page.getByRole('heading')).toContainText('Consultar Pedido');
 
+  //Act
   await page.getByTestId('search-order-id').fill('VLO-H0AEWJ');
-
   await page.getByTestId('search-order-button').click();
+
+  //Assert
   await expect(page.getByTestId('order-result-id')).toBeVisible();
   await expect(page.getByTestId('order-result-id')).toContainText('VLO-H0AEWJ');
-  
+
   await expect(page.getByTestId('order-result-status')).toBeVisible();
   await expect(page.getByTestId('order-result-status')).toContainText('APROVADO');
 });
